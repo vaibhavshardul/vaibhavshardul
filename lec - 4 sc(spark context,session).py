fdad72303dcc1bfbaa75a883a4b99f0d5ac9c319
vaibhavshardul <vaibhavshardul9096@gmail.com>
2025-09-05 23:47:20 +0530

    Add files via upload
    spark file

---
+++ lec - 4 sc(spark context,session).py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+""" 
+1)spark context  =   context =manages cluster and assign task to executer
+2)executor       =      Workers that acutally process data in the cluster
+3)sparak session = a user-specific environmrnt for running queries """
+
+# ONE SPARK CONTEXT HAS MANY SPARK SESSION
+
+# COMMAND ----------
+
+
